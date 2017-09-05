395700ca7a307b162df87e0d05b37e0547a64f58
loezzje <m.demmendaal@hotmail.com>
2017-09-05 16:22:52 +0200

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first
 User.destroy_all
 
-marloes = User.create(email: "marloes@email.com", password: "ThisPassword")
+marloes = User.create!(email: "marloes@email.com", password: "ThisPassword")
 
-event1 = Event.create(name: "Cycling", description: "Cycle to the beach", location: "Haarlem", price: 2.00, capacity: 10, includes_food: true, includes_drinks: true, starts_at: 2017-04-02, ends_at: 2017-04-02, user: marloes)
-event1 = Event.create(name: "Hiking", description: "Hike to the park", location: "Amsterdam", price: 10.00, capacity: 20, includes_food: true, includes_drinks: false, starts_at: 2017-04-02, ends_at: 2017-04-02, user: marloes)
-event1 = Event.create(name: "Drink Beer", description: "Enjoy your friday afternoon with a cold one", location: "Bar", price: 10.00, capacity: 50, includes_food: false, includes_drinks: true, starts_at: 2017-04-02, ends_at: 2017-04-02, user: marloes)
+event1 = Event.create!(name: "Cycling", description: "Cycle to the beach", location: "Haarlem", price: 2.00, capacity: 10, includes_food: true, includes_drinks: true, starts_at: "2017-04-02", ends_at: "2017-04-02", user: marloes)
+event2 = Event.create!(name: "Hiking", description: "Hike to the park", location: "Amsterdam", price: 10.00, capacity: 20, includes_food: true, includes_drinks: false, starts_at: "2017-04-02", ends_at: "2017-04-02", user: marloes)
+event3 = Event.create!(name: "Drink Beer", description: "Enjoy your friday afternoon with a cold one", location: "Bar", price: 10.00, capacity: 50, includes_food: false, includes_drinks: true, starts_at: "2017-04-02", ends_at: "2017-04-02", user: marloes)
