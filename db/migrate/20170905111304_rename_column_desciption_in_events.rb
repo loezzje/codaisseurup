a5a269d05aa268723abb5a9b80b535cb43d96620
loezzje <m.demmendaal@hotmail.com>
2017-09-05 16:09:46 +0200

    add seeds and fix database. I hope

--- db/migrate/20170905111304_rename_column_desciption_in_events.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnDesciptionInEvents < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :events, :desciption, :description
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,11 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
+User.destroy_all
+
+marloes = User.create(email: "marloes@email.com", password: "ThisPassword")
+
+event1 = Event.create(name: "Cycling", description: "Cycle to the beach", location: "Haarlem", price: 2.00, capacity: 10, includes_food: true, includes_drinks: true, starts_at: 2017-04-02, ends_at: 2017-04-02, user: marloes)
+event1 = Event.create(name: "Hiking", description: "Hike to the park", location: "Amsterdam", price: 10.00, capacity: 20, includes_food: true, includes_drinks: false, starts_at: 2017-04-02, ends_at: 2017-04-02, user: marloes)
+event1 = Event.create(name: "Drink Beer", description: "Enjoy your friday afternoon with a cold one", location: "Bar", price: 10.00, capacity: 50, includes_food: false, includes_drinks: true, starts_at: 2017-04-02, ends_at: 2017-04-02, user: marloes)
